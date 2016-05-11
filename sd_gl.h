31d08a3694b69bc40fddc104835986f344110f7f
pjsavola <petri@netcore.fi>
2016-05-11 15:46:37 +0300

    Add support for rendering text, needs a bit (or actually quite much) of cleaning

--- sd_gl.h
+++ sd_gl.h
@@ -6,8 +6,13 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/gtx/transform.hpp>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 #include <vector>
 #include <string>
+#include <map>
 
 struct GLObjectData {
     std::vector<glm::vec3> vertices;
@@ -27,6 +32,29 @@ private:
     GLuint colorbuffer;
 };
 
+class FontRenderer {
+private:
+    struct Character {
+        GLuint     TextureID;  // ID handle of the glyph texture
+        glm::ivec2 Size;       // Size of glyph
+        glm::ivec2 Bearing;    // Offset from baseline to left/top of glyph
+        GLuint     Advance;    // Offset to advance to next glyph
+    };
+
+    std::map<GLchar, Character> Characters;
+
+    FT_Library ft;
+    FT_Face face;
+    GLuint VBO;
+    GLuint VAO;
+    GLuint program;
+
+public:
+    ~FontRenderer();
+    void Init();
+    void RenderText(std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color) const;
+};
+
 class Program {
 public:
     Program(int width, int height, const std::string &title);
@@ -39,6 +67,7 @@ private:
     GLuint programID;
     GLuint MatrixID;
     bool initialized;
+    FontRenderer fontRenderer;
 };
 
 #endif // SD_GL_H
